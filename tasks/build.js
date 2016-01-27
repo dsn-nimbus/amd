2ca0bed9bb30e2ebc5a674aece0f63dabd460c9e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-01-27 09:57:34 -0200

    faz com que task de build da versao 3 do bootstrap delete a pasta de dist antes

--- tasks/build.js
+++ tasks/build.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import minCss from 'gulp-minify-css';
 import * as constantes from './utils/const';
 
-gulp.task('build-bs-v3', () => {
+gulp.task('build-bs-v3', ['del-dist-bs-v3'], () => {
   const ARQUIVOS_SRC_BOOTSTRAP_CSS = constantes.bootstrap.v3.srcArquivosCss;
   const CAMINHO_DIST_BOOTSTRAP_3 = constantes.bootstrap.v3.dist;
 
