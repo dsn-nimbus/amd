3c13a6780181c841a863449d325f5a8b23107ea9
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-06-27 08:00:00 -0300

    .

--- bootstrap/3.x/documentacao/exemplo/_dependencias/material/js/app.js
+++ bootstrap/3.x/documentacao/exemplo/_dependencias/material/js/app.js
@@ -31,5 +31,29 @@
         .otherwise({redirectTo: '/'})
 
       $locationProvider.html5Mode(true);
+    }])
+    .controller('CabecalhoController', [function() {
+      this.usuario = {
+        imagem: 'https://avatars0.githubusercontent.com/u/7040162?v=3&s=4i0',
+        nome: 'Fulano da Silva',
+        email: 'fulanosilva@gmail.com'
+      }
+
+      this.produtos = [
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+        {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'}
+      ]
     }]);
 }(angular));
