fdeec74a5c519dd574ef1974a22727f0d8601acc
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-06-27 09:16:26 -0300

    .

--- bootstrap/3.x/documentacao/exemplo/_dependencias/material/js/app.js
+++ bootstrap/3.x/documentacao/exemplo/_dependencias/material/js/app.js
@@ -32,12 +32,22 @@
 
       $locationProvider.html5Mode(true);
     }])
-    .controller('CabecalhoController', [function() {
+    .controller('CabecalhoController', ['$rootScope', '$location', function($rootScope, $location) {
       this.usuario = {
         imagem: 'https://avatars0.githubusercontent.com/u/7040162?v=3&s=4i0',
         nome: 'Fulano da Silva',
         email: 'fulanosilva@gmail.com'
       }
+
+      this.rotas = [
+        {nome: 'Tabelas', path: '/tabelas', ativa: false},
+        {nome: 'Modais', path: '/modais', ativa: false},
+        {nome: 'Abas', path: '/abas', ativa: false},
+        {nome: 'Cards', path: '/cards', ativa: false},
+        {nome: 'Listas', path: '/listas', ativa: false},
+        {nome: 'Forms', path: '/forms', ativa: false},
+        {nome: 'Loading', path: '/loading', ativa: false},
+      ]
 
       this.produtos = [
         {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
@@ -55,5 +65,20 @@
         {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
         {nome: 'Nome do Produto', logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'}
       ]
+
+      this._selecionaRota = function() {
+        var _path = $location.path();
+
+        this.rotas.forEach(function(rota) {
+          rota.ativa = false;
+
+          if (_path === rota.path) {
+            rota.ativa = true;
+          }
+        });
+      }
+
+      $rootScope.$on('$viewContentLoaded', this._selecionaRota.bind(this));
+      $rootScope.$on('$locationChangeSuccess', this._selecionaRota.bind(this));
     }]);
 }(angular));
